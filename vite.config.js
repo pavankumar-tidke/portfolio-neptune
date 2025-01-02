5a6796860cbec2fe9046c3b5688177c4c86c722a
pavankumar-tidke <pavankumartidke12345@gmail.com>
2025-01-02 21:09:12 +0530

    img1 added rollup

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  rollupOptions: {
+    external: [
+      './assets/images/projects/img1.png' // Add your specific file or pattern here
+    ],
+  },
 })
